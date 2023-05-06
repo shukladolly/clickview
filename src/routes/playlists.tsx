c2fad42bed2f4707e8bba393ca03fb486b5d7c4e
Dolly <dollyshukla321@gmail.com>
2023-05-06 12:14:42 +1000

    completed 3 fully, doing the 4th one partially done

--- src/routes/playlists.tsx
+++ src/routes/playlists.tsx
@@ -1,9 +1,13 @@
 import playlistData from '../playlists.json';
+import videolistData from '../videos.json';
 import { PlaylistItem } from '../components/playlist-item';
+import { Link } from 'react-router-dom';
+
 import { SetStateAction, useState } from 'react';
 export function Playlists() {
   const [playlists, setPlaylists] = useState(playlistData);
   const [inputValue, setInputValue] = useState('');
+  const [w, setInputVidos] = useState(videolistData);
   const [editingIndex, setEditingIndex] = useState(-1);
 
   const addPlaylists = () => {
@@ -31,6 +35,9 @@ export function Playlists() {
     setInputValue(event.target.value);
   };
 
+  const handleInputVideo = () => {
+    return "tet"
+  }
 
 
   return (
@@ -40,18 +47,17 @@ export function Playlists() {
         playlists.map((playListItem, index) => (
           <div>
           <PlaylistItem playlist={playListItem} />
-          <button onClick={() => handleDeleteChange(index)}>Delete</button>
+          <button className='mb-0' onClick={() => handleDeleteChange(index)}>Delete Playlist</button>
           </div>
         )
         )
       
       } 
       <div>
-        <button onClick={addPlaylists}> Add Playlist</button>
+        {/* <button onClick={addPlaylists}> Add Playlist</button> */}
         <div>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={addPlaylists}>Add</button>
-         
+        <input type="text" placeholder = " Name of Playlist" value={inputValue} onChange={handleInputChange} />
+      <button onClick={addPlaylists}>Add New Playlist</button>
         </div>
       </div>
       </div>
--- src/videos.tsx
+++ src/videos.tsx
@@ -1,14 +1,41 @@
 import videoData from './videos.json'
+import playlistData from './playlists.json';
 import VideoItem from './components/video-item';
+import { SetStateAction, useState } from 'react';
+
 export function Videos() {
+
+
+  const [selectedPlayList, setSelectedPlayList] = useState('');
+
+  const handlePlayListChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setSelectedPlayList(event.target.value)
+  };
+
+  // const addVideoToPlayList = () => {
+
+  // }
+
   return (
     <main>
       {
         videoData.map((video, index) => (
+          <div>
           <VideoItem video={video} />
-        )
-        )
       
+
+    <select id="dropdown" value={selectedPlayList} onChange={handlePlayListChange}>
+        <option value="">--Please add to a playlist option--</option>
+        {playlistData.map((option, index) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+          {/* <button onClick={addVideoToPlayList}>Add video to playlist</button> */}
+          </div>
+        )
+        )
       } 
     </main>
   )
